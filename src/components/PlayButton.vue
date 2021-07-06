5f13766d3ca428cb1aef3e96d727eb9d965c7064
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-07-06 11:27:31 +0200

    add PlayButton component

---
+++ src/components/PlayButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button class="play-button" :class="`color-variant--${variant}`">
+    <div class="play-button__icon-container">
+      <PlayButtonIcon :variant="variant" />
+    </div>
+  </button>
+</template>
+
+<script>
+import PlayButtonIcon from "./PlayButtonIcon.vue";
+export default {
+  components: { PlayButtonIcon },
+  props:{
+    variant :{
+      type:String,
+      default:'scissors'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@mixin circle($size){
+  width:$size;
+  height:$size;
+  border-radius: 50%;
+
+}
+
+.play-button {
+  @include circle(8rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border:none;
+ 
+  &__icon-container{
+    @include circle(6rem);
+    background-color: var(--color-white);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+}
+$variants : 'scissors', 'rock', 'paper', 'lizard', 'spook';
+
+@each $variant in $variants {
+  .color-variant--#{$variant}{
+    background-image: var(--gradient-#{$variant});
+
+  }
+} 
+</style>
