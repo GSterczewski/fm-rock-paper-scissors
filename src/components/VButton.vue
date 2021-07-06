6004ddeb3f2ce2c3155aef10930f05c62c0f5351
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-07-06 09:31:14 +0200

    add Button component

---
+++ src/components/VButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <button class="button button--outline button--rounded" @click="clickHandler" v-text="name"></button>
+</template>
+
+<script>
+export default {
+  props:{
+    name: {
+      type:String,
+      default : "Click me"
+    },
+    clickHandler: {
+      type: Function,
+      default: () => {console.warn("click handler not provided!")}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .button {
+    padding: 0.7rem 2.35rem;
+    text-transform: uppercase;
+    font-weight: 700;
+    cursor:pointer;
+  }
+  .button--outline{
+    background-color: transparent;
+    border: 1px solid var(--color-white);
+    color:var(--color-white);
+    transition: 0.3s ease-out;
+    
+    &:hover, 
+    &:focus{
+      outline:none;
+      background-color: var(--color-white);
+      color: var(--color-score-text);
+    }
+  }
+
+  .button--rounded {
+    border-radius:0.55em;
+  }
+</style>
